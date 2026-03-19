03b139af13db5ba1d466297110806c37bf361396
juliuszbrzeziak <Juliuszbrzeziak@gmail.com>
2026-03-20 00:43:30 +0100

    conversationTask

---
+++ multi-agent-support/src/main/java/com/juliusz/ConversationTask.java
@@ -0,0 +1,51 @@
+public class ConversationTask {
+
+    public enum TaskStatus {
+        NEW,           // orchiestrator
+        IN_PROGRESS,   // triage agent
+        DONE           // done
+    }
+
+    public enum TaskCategory {
+        TRIAGE,
+        TECHNICAL,
+        BILLING
+    }
+
+    private final int id;
+    private final String rawText;
+
+    private TaskStatus status;
+    private TaskCategory category;
+
+    public ConversationTask(int id, String rawText) {
+        this.id = id;
+        this.rawText = rawText;
+        this.status = TaskStatus.NEW;
+        this.category = TaskCategory.TRIAGE;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRawText() {
+        return rawText;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(TaskCategory category) {
+        this.category = category;
+    }
+}
